2c0bb19024fb935736fdb41924d306a7ed2fba42
Juliandr Kendzikundu D'Amar Hoyamovich III <vladik-g2911shangyang@mail.ru>
2022-06-30 19:25:50 +0300

    Выполнил первое задание. Надеюсь, правильно понял его суть.

---
+++ Task1/MyException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+internal class MyException : Exception
+{
+    public override string Message { get; }
+    internal MyException() : base()
+    {
+        Message = "Сработала некая ошибка";
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Exception[] exceptions = { new ApplicationException(), new ArgumentException(), new ArgumentNullException(), new ArgumentOutOfRangeException(), new MyException()};
+ 
+try
+{
+    throw new MyException();
+}
+catch (Exception ex) when (ex.GetType() == exceptions[0].GetType())
+{
+
+}
+catch (Exception ex) when (ex.GetType() == exceptions[1].GetType())
+{
+
+}
+catch (Exception ex) when (ex.GetType() == exceptions[2].GetType())
+{
+
+}
+catch (Exception ex) when (ex.GetType() == exceptions[3].GetType())
+{
+
+}
+catch (Exception ex) when (ex.GetType() == exceptions[4].GetType())
+{
+    Console.WriteLine(ex.Message);
+}
